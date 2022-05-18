caa74b5641cef3b1b34c27561c7df867a38f761e
rezwan <rezwan@yotech.ltd>
2022-05-18 15:17:59 +0600

    implementd password hasing successfully

--- api/models/employee.js
+++ api/models/employee.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const employeeSchema = mongoose.Schema({
     name: {
@@ -9,16 +10,49 @@ const employeeSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    password: {
-        type: String,
-        required: true
-    },
     isAdmin: {
         type: Boolean,
         required: false,
         default: false
     },
-    image: String
+    image: {
+        type: String,
+        required: false
+    },
+    hash: String,
+    salt: String
 })
+
+
+// Method to set salt and hash the password for a user
+// setPassword method first creates a salt unique for every user
+// then it hashes the salt with user password and creates a hash
+// this hash is stored in the database as user password
+employeeSchema.methods.setPassword = function(password) {     
+    // Creating a unique salt for a particular user
+       this.salt = crypto.randomBytes(16).toString('hex');
+     
+       // Hashing user's salt and password with 1000 iterations, 64 length and sha512 digest
+       this.hash = crypto.pbkdf2Sync(password, this.salt, 
+       1000, 64, `sha512`).toString(`hex`);
+   };
+
+
+// Method to check the entered password is correct or not
+// valid password method checks whether the user
+// password is correct or not
+// It takes the user password from the request 
+// and salt from user database entry
+// It then hashes user password and salt
+// then checks if this generated hash is equal
+// to user's hash in the database or not
+// If the user's hash is equal to generated hash 
+// then the password is correct otherwise not
+employeeSchema.methods.validPassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, 
+    this.salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.hash === hash;
+};
+
 
 module.exports = mongoose.model('Employee', employeeSchema, 'employee-collc');
--- api/routes/employee.js
+++ api/routes/employee.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // imported user schema
 const Employee = require('../models/employee');
@@ -57,6 +58,61 @@ router.post('/', async (req, res, next) => {
         })
     }
 });
+
+
+// user login api
+router.post('/login', (req, res, next) => {
+
+    // find user with requested email
+    Employee.findOne({email: req.body.email}, function(err, user) {
+        if(user === null){
+            return res.status(400).json({
+                message : "User not found."
+            });
+        }
+        else {
+            if(user.validPassword(req.body.password)) {
+                return res.status(201).json({
+                    message: "User logged in"
+                })
+            }
+            else{
+                return res.status(400).send({
+                    message: "Wrong password"
+                })
+            }
+        }
+    })
+})
+
+
+// user signup api
+router.post('/signup', (req, res, next) => {
+    // creating empty user object
+    let newUser = new Employee();
+
+    // initialize newUser object with request data
+    newUser.name = req.body.name
+    newUser.email = req.body.email
+
+    // call setPassword function to hash password
+    newUser.setPassword(req.body.password)
+
+    // save newUser object to database
+    newUser.save((err, Employee) => {
+        if(err){
+            return res.status(400).json({
+                message: "Failed to add user."
+            });
+        }
+        else {
+            return res.status(201).json({
+                message: "User added successfully"
+            })
+        }
+    })
+})
+
 
 // update an employee
 router.patch('/:empId', async (req, res, next) => {
