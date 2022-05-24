0ae461255507b514d01e6b259800325cd1984d06
rezwan <rezwan@yotech.ltd>
2022-05-24 12:59:55 +0600

    code cleaned

--- api/middlewares/verifyAdmin.js
+++ api/middlewares/verifyAdmin.js
@@ -1,23 +1,7 @@
 const Employee = require('../models/employee');
 jwt = require('jsonwebtoken');
-const jwtAuth = require('../middlewares/jwtAuth');
 
-module.exports = {
-    /* verifyJWT: async (req, res, next) => {
-        const authHeader = await req.headers.authorization;
-        if (!authHeader) {
-          return res.status(401).send({ message: 'UnAuthorized access' });
-        }
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-          if (err) {
-            return res.status(403).send({ message: 'Forbidden access' })
-          }
-          req.decoded = decoded;
-          next();
-        });
-      }, */
-    
+module.exports = {    
     isAdmin: async (req , res, next) => {  
         const requester = req.decoded ? req.decoded.id : undefined;
         const requesterAccount = await Employee.findOne({_id: requester});
